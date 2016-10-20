38b32a6a9e11841d62bac58f934be479bde60dd9
zPhumzani <zphumzani@gmail.com>
2016-10-20 20:17:47 +0200

    create the main app class

--- lib/wew3aiot.rb
+++ lib/wew3aiot.rb
@@ -1,5 +1,23 @@
 require "wew3aiot/version"
 
 module Wew3aiot
-  # Your code goes here...
+  class Application
+    def call(env)
+      @req = Rack::Request.new(env)
+      path = @req.path_info
+      request_method = @req.request_method.downcase
+      return [500, {}, []] if path == "/favicon.ico"
+      controller, action = fetch_controller_and_action(path, request_method)
+      response = controller.new.send(action)
+      [200, {"Content-Type" => "text/html"}, [response]]
+    end
+
+    def fetch_controller_action(path, verb)
+      _, controller, action, others = path.split("/", 4)
+      require "#{controller.downcase}_controller.rb"
+      controller = Object.const_get(controller.capitalize! + "Controller")
+      action = action.nil? ? verb : "#{verb}_#{action}"
+      [controller, action]
+    end
+  end
 end
